c6f6d7b890b283099f3375a79bd7e3c8c1ca27d6
nirdeshacharya <nirdesh.acharya@hotmail.com>
2022-04-24 19:50:15 +0100

    Added new test case

--- TrueLayerChallenge.Tests/PokemonControllerTest.cs
+++ TrueLayerChallenge.Tests/PokemonControllerTest.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using FluentAssertions;
@@ -6,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using TrueLayerChallenge.Controllers;
 using TrueLayerChallenge.Interface;
+using TrueLayerChallenge.Models;
 using TrueLayerChallenge.Services;
 using Xunit;
 
@@ -13,48 +15,77 @@ namespace TrueLayerChallenge.Tests
 {
 	public class PokemonControllerTest
 	{
-        readonly IPokemonDetailsService _pokemonDetail;
-        readonly IPokemonDetailsTranslatedService _pokemonTranslatedDetail;
+        private readonly IPokemonDetailsService _pokemonDetail;
+        private readonly IPokemonDetailsTranslatedService _pokemonTranslatedDetail;
+        private readonly CancellationToken  cts;
+
+        private readonly ILogger<PokemonController> logger;
+
         public PokemonControllerTest()
 		{
             _pokemonDetail = new PokemonDetailsService();
             _pokemonTranslatedDetail = new PokemonDetailsTranslatedService();
+            cts = new CancellationToken();
+            var loggerFactory = new LoggerFactory();
+            logger = loggerFactory.CreateLogger<PokemonController>();
         }
+
         [Fact]
-        public async Task GetTranslated_ShouldReturn200Status()
+        public async Task GetTranslated_ShouldReturn200Status_When_ActualPokemonIsInput()
         {
             // Arrange
-            var cts = new CancellationToken();
             var pokemonName = "pikachu";
-            var loggerFactory = new LoggerFactory();
-            ILogger<PokemonController> logger = loggerFactory.CreateLogger<PokemonController>();
+   
             var controller = new PokemonController(logger, _pokemonDetail, _pokemonTranslatedDetail);
 
             // Act
             var result = (OkObjectResult)await controller.GetTranslated(pokemonName, cts);
 
-
             // Assert
+            Assert.NotNull(result);
             result.StatusCode.Should().Be(200);
         }
 
         [Fact]
-        public async Task GetTranslated_ShouldReturn404Status()
+        public async Task GetTranslated_ShouldReturn404Status_When_RandomPokemonIsInput()
         {
             // Arrange
-            var cts = new CancellationToken();
             var pokemonName = "random";
-            var loggerFactory = new LoggerFactory();
-            ILogger<PokemonController> logger = loggerFactory.CreateLogger<PokemonController>();
+          
             var controller = new PokemonController(logger, _pokemonDetail, _pokemonTranslatedDetail);
 
             // Act
-            var result = (OkObjectResult)await controller.GetTranslated(pokemonName, cts);
+            var result = await controller.GetTranslated(pokemonName, cts);
 
 
             // Assert
-            result.StatusCode.Should().Be(404);
+            Assert.NotNull(result);
+            result.Should().BeOfType<NotFoundResult>();
         }
 
+        [Fact]
+        public async Task GetTranslated_ShouldReturnPokemonName_When_InputPokemonName()
+        {
+            // Arrange
+            var pokemonNameList = new List<string> {
+                "charizard", "pikachu", "charmander"
+            };
+
+            var controller = new PokemonController(logger, _pokemonDetail, _pokemonTranslatedDetail);
+
+            
+            foreach(var pokemonName in pokemonNameList)
+			{
+                // Act
+                var result = (OkObjectResult)await controller.GetTranslated(pokemonName, cts);
+                Assert.NotNull(result);
+
+
+                // Assert
+                var actualResult = result.Value.As<PokemonSummary>();
+                Assert.NotNull(actualResult);
+                result.Value.As<PokemonSummary>().Name.Equals(pokemonName);
+            }
+        }
     }
 }
